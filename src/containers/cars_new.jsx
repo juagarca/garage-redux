f45d35bc43d8b4068c78fcf69040e825654574fb
Juan Garcia <juagarca@gmail.com>
2021-01-06 12:34:24 +0000

    added image and overlay to the form container background

--- src/containers/cars_new.jsx
+++ src/containers/cars_new.jsx
@@ -21,10 +21,12 @@ class CarsNew extends Component {
       <Sidebar key="sidebar" name={this.props.garageName}>
         <Link to="/" className="btn btn-primary">Back</Link>
       </Sidebar>,
-      <div className="form-container">
+      <div className="form-container" style={{ backgroundImage: "url('/assets/images/garage.png')" }}>
+        <div className="overlay"></div>
         <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
           <label htmlFor="InputBrand">Brand</label>
           <Field
+            id="InputBrand"
             label="Brand"
             name="brand"
             type="text"
@@ -33,6 +35,7 @@ class CarsNew extends Component {
           />
           <label htmlFor="InputModel">Model</label>
           <Field
+            id="InputModel"
             className="form-control"
             label="Model"
             name="model"
@@ -41,6 +44,7 @@ class CarsNew extends Component {
           />
           <label htmlFor="InputOwner">Owner</label>
           <Field
+            id="InputOwner"
             className="form-control"
             label="Owner"
             name="owner"
@@ -49,17 +53,20 @@ class CarsNew extends Component {
           />
           <label htmlFor="InputPlate">Plate</label>
           <Field
+            id="InputPlate"
             className="form-control"
             label="Plate"
             name="plate"
             type="text"
             component="input"
           />
-          <button
-            className="btn btn-primary"
-            type="submit"
-            disabled={this.props.pristine || this.props.submitting}
-          >Create Car</button>
+          <div className="text-center">
+            <button
+              className="btn btn-primary"
+              type="submit"
+              disabled={this.props.pristine || this.props.submitting}
+            >Create Car</button>
+          </div>
         </form>
       </div>
     ];
