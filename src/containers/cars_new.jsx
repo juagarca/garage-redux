6c3fec6a90da2895e915c78a0cd4bd8513def7e3
Juan Garcia <juagarca@gmail.com>
2021-01-06 11:56:23 +0000

    fixed form container dimensions

--- src/containers/cars_new.jsx
+++ src/containers/cars_new.jsx
@@ -21,45 +21,47 @@ class CarsNew extends Component {
       <Sidebar key="sidebar" name={this.props.garageName}>
         <Link to="/" className="btn btn-primary">Back</Link>
       </Sidebar>,
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-        <label htmlFor="InputBrand">Brand</label>
-        <Field
-          label="Brand"
-          name="brand"
-          type="text"
-          className="form-control"
-          component="input"
-        />
-        <label htmlFor="InputModel">Model</label>
-        <Field
-          className="form-control"
-          label="Model"
-          name="model"
-          type="text"
-          component="input"
-        />
-        <label htmlFor="InputOwner">Owner</label>
-        <Field
-          className="form-control"
-          label="Owner"
-          name="owner"
-          type="text"
-          component="input"
-        />
-        <label htmlFor="InputPlate">Plate</label>
-        <Field
-          className="form-control"
-          label="Plate"
-          name="plate"
-          type="text"
-          component="input"
-        />
-        <button
-          className="btn btn-primary"
-          type="submit"
-          disabled={this.props.pristine || this.props.submitting}
-        >Create Car</button>
-      </form>
+      <div className="form-container">
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
+          <label htmlFor="InputBrand">Brand</label>
+          <Field
+            label="Brand"
+            name="brand"
+            type="text"
+            className="form-control"
+            component="input"
+          />
+          <label htmlFor="InputModel">Model</label>
+          <Field
+            className="form-control"
+            label="Model"
+            name="model"
+            type="text"
+            component="input"
+          />
+          <label htmlFor="InputOwner">Owner</label>
+          <Field
+            className="form-control"
+            label="Owner"
+            name="owner"
+            type="text"
+            component="input"
+          />
+          <label htmlFor="InputPlate">Plate</label>
+          <Field
+            className="form-control"
+            label="Plate"
+            name="plate"
+            type="text"
+            component="input"
+          />
+          <button
+            className="btn btn-primary"
+            type="submit"
+            disabled={this.props.pristine || this.props.submitting}
+          >Create Car</button>
+        </form>
+      </div>
     ];
   }
 }
